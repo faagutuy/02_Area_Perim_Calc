9eb8bd2abb84f0c2afb1dc585700d97f737a10e2
faagutuy <faagutuy1446@masseyhigh.school.nz>
2021-09-08 13:10:15 +1200

    Update 01_looping_basic_v2.py

--- 01_looping_basic_v2.py
+++ 01_looping_basic_v2.py
@@ -2,14 +2,23 @@
 valid = False
 while not valid:
 
-    # ask user to enter a number
-    response = float(input("Enter a number: "))
-    
-    # checks number is more than zero
-    if response > 0:
-        valid = True
-    
-    # outputs error if input is invalid
-    else: 
-        print("Please enter a number that is more than zero")
-        print()
+    error = "Please enter a number that is more than zero"
+
+    try:
+
+        # ask user to enter a number
+        response = float(input("Enter a number: "))
+        
+        # checks number is more than zero
+        if response > 0:
+            valid = True
+        
+        # outputs error if input is invalid
+        else: 
+            print(error)
+            print()
+
+    except ValueError:
+        print(error)
+
+
