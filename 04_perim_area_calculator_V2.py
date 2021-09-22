fde28a3dc25160792a9f9f844ed1d8963cac164c
faagutuy <faagutuy1446@masseyhigh.school.nz>
2021-09-22 13:15:40 +1200

    Update 04_perim_area_calculator_V2.py

--- 04_perim_area_calculator_V2.py
+++ 04_perim_area_calculator_V2.py
@@ -47,8 +47,8 @@ while keep_going == "":
     perimeter = 2 * (width + height)
 
     # Output area and perimeter
-    print("Perimeter: {} units".format(perimeter))
-    print("Area: {} square units". format(area))
+    print("Perimeter: {:2f} units".format(perimeter))
+    print("Area: {:2f} square units". format(area))
     print ()
 
     keep_going = input("Press <enter> to keep going or any key to quit")
